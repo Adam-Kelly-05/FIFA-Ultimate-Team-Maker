39dd7fbb3e60e649baf7143d33a33f03e74ef00c
AdamKelly-STUDENT <AdamKelly-STUDENT@S-4P1H504-SL10>
2025-03-05 15:35:59 +0000

    Add project files.

---
+++ FIFA Team Maker/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System.Net.Http;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Newtonsoft.Json.Linq;
+
+namespace FIFA_Team_Maker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Search_BTN_Click(object sender, RoutedEventArgs e)
+        {
+            string search = SearchBar.Text;
+            SearchAPI(search);
+        }
+
+        private async Task SearchAPI(string search)
+        {
+            string apiKey = "496d47c537f35ec7b40b14859f19a74e";
+            string apiUrl = $"https://v3.football.api-sports.io/players/profiles?search={search.Replace(" ", "%20")}";
+
+            using HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("x-rapidapi-key", apiKey);
+            HttpResponseMessage response = await client.GetAsync(apiUrl);
+            string responseString = await response.Content.ReadAsStringAsync();
+
+            JObject responseJSON = JObject.Parse(responseString);
+            Test.Text = responseJSON.ToString();
+
+            JToken player = responseJSON["response"];
+
+            if (player[0] != null)
+            {
+                Player1Border_BDR.BorderBrush = Brushes.Black;
+                Player1Name_TXTBLK.Text = player[0]["player"]["name"].ToString();
+                Player1Country_TXTBLK.Text = player[0]["player"]["nationality"].ToString();
+                Player1Age_TXTBLK.Text = player[0]["player"]["age"].ToString();
+                Player1Image_IMG.Source = new BitmapImage(new Uri(player[0]["player"]["photo"].ToString()));
+                Player1Position_TXTBLK.Text = player[0]["player"]["position"].ToString();
+            }
+
+            if (player[1] != null)
+            {
+                Player2Border_BDR.BorderBrush = Brushes.Black;
+                Player2Name_TXTBLK.Text = player[1]["player"]["name"].ToString();
+                Player2Country_TXTBLK.Text = player[1]["player"]["nationality"].ToString();
+                Player2Age_TXTBLK.Text = player[1]["player"]["age"].ToString();
+                Player2Image_IMG.Source = new BitmapImage(new Uri(player[1]["player"]["photo"].ToString()));
+                Player2Position_TXTBLK.Text = player[1]["player"]["position"].ToString();
+            }
+
+            if (player[2] != null)
+            {
+                Player3Border_BDR.BorderBrush = Brushes.Black;
+                Player3Name_TXTBLK.Text = player[2]["player"]["name"].ToString();
+                Player3Country_TXTBLK.Text = player[2]["player"]["nationality"].ToString();
+                Player3Age_TXTBLK.Text = player[2]["player"]["age"].ToString();
+                Player3Image_IMG.Source = new BitmapImage(new Uri(player[2]["player"]["photo"].ToString()));
+                Player3Position_TXTBLK.Text = player[2]["player"]["position"].ToString();
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double budget = double.Parse(Budget_TXTBX.Text);
+        }
+    }
+}
---
+++ FIFA Team Maker/Player.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Net.Http;
+
+namespace FIFA_Team_Maker
+{
+    class Player
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Nationality { get; set; }
+        public int Age { get; set; }
+        public string Position { get; set; }
+        public string Photo { get; set; } // From Football API
+        public string Team { get; set; }
+
+        private async Task APICalls()
+        {
+            string apiKey = "496d47c537f35ec7b40b14859f19a74e";
+            string apiUrl = $"https://v3.football.api-sports.io/players/teams?player={ID}";
+
+            using HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("x-rapidapi-key", apiKey);
+            HttpResponseMessage response = await client.GetAsync(apiUrl);
+            string responseString = await response.Content.ReadAsStringAsync();
+
+            JObject responseJSON = JObject.Parse(responseString);
+
+            JToken player = responseJSON["response"];
+
+            string team = player["team"]["name"].ToString();
+
+            Console.WriteLine(team);
+        }
+
+        private double price;
+        public double Price
+        {
+            get { return price; }
+            set
+            {
+
+
+                price = value;
+
+                // List of all countries, use players id to search their team, ignore the team if it's in the list of countries
+                // Then use database to fill formula
+            }
+        }
+
+        private double rating;
+        public double Rating
+        {
+            get { return rating; }
+            set
+            {
+                rating = Math.Floor((0.6 * price) + 40); // 40 is the min rating and 100 is the max rating
+            }
+        }
+
+        public Player(string name, string nationality, int age, string position, string photo)
+        {
+            Name = name;
+            Age = age;
+            Nationality = nationality;
+            Position = position;
+            Photo = photo;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}";
+        }
+    }
+}
