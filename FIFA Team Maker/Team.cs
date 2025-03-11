ff3bd8ab8e84f76c2daacf42700b5b15f303d420
Adam Kelly <S00252192@atu.ie>
2025-03-11 14:15:14 +0000

    Created team class for holding players, later will be used to save team and load teams

---
+++ FIFA Team Maker/Team.cs
@@ -0,0 +1,46 @@
+﻿namespace FIFA_Team_Maker
+{
+    class CreatedTeam
+    {
+        public List<Player> Attackers { get; set; }
+        public List<Player> Midfielders { get; }
+        public List<Player> Defenders { get; set; }
+        public List<Player> Goalkeepers { get; set; }
+
+        public void AddPlayer(Player player)
+        {
+            if (player.Position == "Attacker")
+            {
+                if (Attackers.Count < 3)
+                {
+                    Attackers.Add(player);
+                }
+            }
+            else if (player.Position == "Midfielder")
+            {
+                if (Midfielders.Count < 3)
+                {
+                    Midfielders.Add(player);
+                }
+            }
+            else if (player.Position == "Defender")
+            {
+                if (Defenders.Count < 4)
+                {
+                    Defenders.Add(player);
+                }
+            }
+            else if (player.Position == "Goalkeeper")
+            {
+                if (Goalkeepers.Count < 1)
+                {
+                    Goalkeepers.Add(player);
+                }
+            }
+        }
+
+        // for printing to json:
+            // for player in Attacker... for player in Midfielders...
+            // Add rating, name, photo to json
+    }
+}
