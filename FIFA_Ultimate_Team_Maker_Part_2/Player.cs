33a324b69984c03341257ec3eed936f702a1c4a4
Adam Kelly <S00252192@atu.ie>
2025-03-14 13:25:16 +0000

    Moved API calls out of class file and removed unnessicary properties from player class

--- FIFA_Ultimate_Team_Maker_Part_2/Player.cs
+++ FIFA_Ultimate_Team_Maker_Part_2/Player.cs
@@ -1,55 +1,14 @@
-﻿using Newtonsoft.Json.Linq;
-using System;
-using System.Net.Http;
-using System.Threading.Tasks;
+﻿using System;
 using System.Windows.Media.Imaging;
 
 namespace FIFA_Ultimate_Team_Maker_Part_2
 {
-    //TeamData db = new TeamData();
-
     class Player
     {
-        public string ID { get; set; }
         public string Name { get; set; }
         public BitmapImage Photo { get; set; }
         public string Position { get; set; }
-        public string Team { get; set; }
-
-        private async Task APICalls()
-        {
-            string apiKey = "496d47c537f35ec7b40b14859f19a74e";
-            string apiUrl = $"https://v3.football.api-sports.io/players/teams?player={ID}";
-
-            using (HttpClient client = new HttpClient())
-            {
-                client.DefaultRequestHeaders.Add("x-rapidapi-key", apiKey);
-                HttpResponseMessage response = await client.GetAsync(apiUrl);
-                string responseString = await response.Content.ReadAsStringAsync();
-
-                JObject responseJSON = JObject.Parse(responseString);
-
-                JToken player = responseJSON["response"];
-
-                string team = player["team"]["name"].ToString();
-
-                //var query = from t in db.teams
-                //            where 
-            }
-        }
-
-        private double price;
-        public double Price
-        {
-            get { return price; }
-            set
-            {
-                price = 0;
-
-                // List of all countries, use players id to search their team, ignore the team if it's in the list of countries
-                // Then use database to fill formula
-            }
-        }
+        public double Price { get; set; }
 
         private double rating;
         public double Rating
@@ -57,15 +16,16 @@ namespace FIFA_Ultimate_Team_Maker_Part_2
             get { return rating; }
             set
             {
-                rating = Math.Floor((0.6 * price) + 40);
+                rating = Math.Floor((0.6 * Price) + 40);
             }
         }
 
-        public Player(string name, BitmapImage photo, string position)
+        public Player(string name, BitmapImage photo, string position, double price)
         {
             Name = name;
             Photo = photo;
             Position = position;
+            Price = price;
         }
 
         public override string ToString()
